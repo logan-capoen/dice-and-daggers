c476c4bf57cc36ec9d2b0754e7e2eb46704f0230
louishector <louis.hector@epitech.eu>
2026-04-25 19:06:45 +0200

    feat: dynamic bars

--- dice-and-daggers/scenes/bar_handling.gd
+++ dice-and-daggers/scenes/bar_handling.gd
@@ -5,25 +5,18 @@ extends Camera2D
 @onready var hp_joueur = $HPJoueur
 @onready var shield_joueur = $ShieldJoueur
 
-func _input(event):
-	if event.is_action_pressed("z"):
-		animer_barre(hp_joueur, hp_joueur.value + 10)
-	if event.is_action_pressed("s"):
-		animer_barre(hp_joueur, hp_joueur.value - 10)
-	if event.is_action_pressed("e"):
-		animer_barre(shield_joueur, shield_joueur.value + 10)
-	if event.is_action_pressed("d"):
-		animer_barre(shield_joueur, shield_joueur.value - 10)
-	if event.is_action_pressed("r"):
-		animer_barre(hp_boss, hp_boss.value + 10)
-	if event.is_action_pressed("f"):
-		animer_barre(hp_boss, hp_boss.value - 10)
-	if event.is_action_pressed("t"):
-		animer_barre(shield_boss, shield_boss.value + 10)
-	if event.is_action_pressed("g"):
-		animer_barre(shield_boss, shield_boss.value - 10)
+@onready var player = get_tree().get_first_node_in_group("player")
+
+func _process(delta):
+	if player == null:
+		return
+
+	animer_barre(hp_joueur, player.hp_joueur)
+	animer_barre(shield_joueur, player.shield_joueur)
 
 func animer_barre(barre: ProgressBar, valeur: float):
 	valeur = clamp(valeur, barre.min_value, barre.max_value)
 	var tween = create_tween()
-	tween.tween_property(barre, "value", valeur, 0.3).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(barre, "value", valeur, 0.3)\
+		.set_trans(Tween.TRANS_CUBIC)\
+		.set_ease(Tween.EASE_OUT)
